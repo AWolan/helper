e498ee2841452f733a1e51cf52d757e181b47272
awolan <amw.wolan@gmail.com>
2016-01-08 07:02:03 +0100

    change to global

--- client/scripts/obligation/rest.js
+++ client/scripts/obligation/rest.js
@@ -2,13 +2,22 @@
 
 angular
     .module('obligation')
-    .factory('obligationRest', ['$http', function ($http) {
+    .factory('obligationRest', ['$http', '$q', function ($http, $q) {
         return {
             getObligation: function () {
                 return $http.get('/rest/obligation');
             },
             getObligationForMonth: function (year, month) {
-                return $http.get('/rest/obligation/month/' + year + '/' + month);
+//                return $http.get('/rest/obligation/month/' + year + '/' + month);
+                var deferred = $q.defer();
+                $http.get('/rest/obligation/month/' + year + '/' + month)
+                    .then(function (response) {
+                        deferred.resolve(response);
+                    }, function () {
+                        deferred.reject("Error!");
+                    });
+
+                return deferred.promise;
             }
         };
     }]);
--- server/app.js
+++ server/app.js
@@ -1,21 +1,22 @@
 'use strict';
 
-var express, app, managerRest, salaryRest, obligationRest;
+var express, app, managerRest, salaryRest, obligationRest, basePath;
 
 global.serverRequire = function(name) {
     return require(__dirname + '/' + name);
 };
 
 express = require('express');
-app = express(),
-managerRest = serverRequire('/rest/manager'),
-salaryRest = serverRequire('/rest/salary'),
+app = express();
+managerRest = serverRequire('/rest/manager');
+salaryRest = serverRequire('/rest/salary');
 obligationRest = serverRequire('/rest/obligation');
+basePath = __dirname + '/..';
 
-app.use('/styles', express.static('C:/work/my/js/helper/client/styles'));
-app.use('/scripts', express.static('C:/work/my/js/helper/client/scripts/client'));
-app.use('/bower_components', express.static('C:/work/my/js/helper/bower_components'));
-app.use(express.static('C:/work/my/js/helper/client'));
+app.use('/styles', express.static(basePath + '/client/styles'));
+app.use('/scripts', express.static(basePath + '/client/scripts'));
+app.use('/bower_components', express.static(basePath + '/bower_components'));
+app.use(express.static(basePath + '/client'));
 
 app.use('/rest/manager', managerRest);
 app.use('/rest/salary', salaryRest);
